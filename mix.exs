1d76c46d9e5683abf554108473608098e9bf16af
Cam <cjirmas@gmail.com>
2018-04-30 00:51:13 -0700

    Add Hex metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule Bartender.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: "JWT authentication using Absinthe + Guardian",
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: "Cameron Irmas",
+      links: %{"GitHub" => "https://github.com/camirmas/bartender"},
+      source_url: "https://github.com/camirmas/bartender",
     ]
   end
 
